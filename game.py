c7752f59e39b77074b6292475de0d624677786b1
Dan Le <dan.dev947366@gmail.com>
2024-12-06 23:34:07 +0200

    Update

--- game.py
+++ game.py
@@ -3,6 +3,37 @@ from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
 
+class Pathfinder:
+    def __init__(self,matrix):
+        self.matrix = matrix
+        self.grid = Grid(matrix=matrix)
+        self.select_surf = pygame.image.load('selection.png').convert_alpha()
+        # pathhfinding
+        self.path = []
+    def draw_active_cell(self):
+        mouse_pos = pygame.mouse.get_pos()
+        row = mouse_pos[1] // 32
+        col = mouse_pos[0] // 32
+        current_cell_value = self.matrix[row][col]
+        if current_cell_value == 1:
+            rect = pygame.Rect((col*32,row*32),(32,32))
+            screen.blit(self.select_surf, rect)
+
+        print(mouse_pos)
+
+    def create_path(self):
+        # start
+        start_x, start_y = [1,1]
+        start = self.grid.node(start_x, start_y)
+        mouse_pos = pygame.mouse.get_pos()
+        end_x, end_y, = mouse_pos[0] //32, mouse_pos[1] //32
+        end = self.grid.node(end_x,end_y)
+        finder = AStarFinder(diagonal_movement=DiagonalMovement)
+        self.path, =finder.find_path(start, end, self.grid)
+        print(self.path)
+    def update(self):
+        self.draw_active_cell()
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 736))
 clock = pygame.time.Clock()
@@ -29,11 +60,15 @@ matrix = [
 	[0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0],
 	[0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+pathfinder = Pathfinder(matrix)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.blit(gb_surf, (0,0))
+    screen.blit(bg_surf, (0,0))
+    pathfinder.draw_active_cell()
     pygame.display.update()
     clock.tick(60)
